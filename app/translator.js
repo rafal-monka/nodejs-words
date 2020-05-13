76b621121ce9a40a6b3639044752b23c5d0d0b5f
Rafal Monka <monka.rafal@gmail.com>
2020-05-13 15:17:12 +0200

    translator

--- app/translator.js
+++ app/translator.js
@@ -17,7 +17,7 @@ exports.getTranslation = (phrase) => {
     }
 }
 
-exports.parse = (html) => {    
+exports.parseSimple = (html) => {    
     var parser = new DomParser();
     var dom = parser.parseFromString(html);
     let translations = [];
@@ -31,6 +31,72 @@ exports.parse = (html) => {
     return translations;
 }
 
+exports.parse = (html) => {    
+    var parser = new DomParser();
+    var dom = parser.parseFromString(html);
+    var translations = [];
+
+    try {
+        var dictionaryEntities = dom.getElementsByClassName('diki-results-left-column')[0].getElementsByClassName('dictionaryEntity');	
+        //console.log(dictionaryEntities);				
+        for (var de = 0; de < dictionaryEntities.length; de++) {	
+            //console.log(dictionaryEntities[de]);
+            
+            var partOfSpeech = dictionaryEntities[de].getElementsByClassName('partOfSpeech');
+            //console.log("partOfSpeech", partOfSpeech[0].innerHTML.trim());
+            var obj = {};
+            obj.part = partOfSpeech[0].innerHTML.trim().replace('&nbsp;',' ');
+            
+            var hws = dictionaryEntities[de].getElementsByClassName('hws')[0].getElementsByClassName('hw');
+            obj.hws = [];
+            for (var h = 0; h < hws.length; h++) {
+                let txt = hws[h].innerHTML.trim().replace(/<[^>]*>?/gm, '').replace(/\s\s+/g, ' ');
+                obj.hws.push(txt);
+            }
+            
+            var foreignToNativeMeanings = dictionaryEntities[de].getElementsByClassName('foreignToNativeMeanings');
+            for (var fn = 0; fn < foreignToNativeMeanings.length; fn++) {
+                
+                obj.meanings = [];
+                var plainLinks = foreignToNativeMeanings[fn].getElementsByClassName('plainLink');						
+                for (var pl = 0; pl < plainLinks.length; pl++) {
+                    let txt = plainLinks[pl].innerHTML.trim().replace(/<[^>]*>?/gm, '').replace(/\s\s+/g, ' ');
+                    //console.log(pl, txt);
+                    obj.meanings.push(txt);
+                }
+                
+                obj.examples = [];
+                var exampleSentences = foreignToNativeMeanings[fn].getElementsByClassName('exampleSentence');
+                for (var es = 0; es < exampleSentences.length; es++) {
+                    let txt = exampleSentences[es].innerHTML.trim().replace(/<[^>]*>?/gm, '').replace(/\s\s+/g, ' ')							
+                    //console.log(es, txt);
+                    obj.examples.push(txt);
+                }
+            }
+            translations.push(obj);
+        }
+        console.log(JSON.stringify(translations));
+        console.log(translations);
+        return translations;
+    } catch (e) {
+        console.log(e.toString());
+        console.error();
+    }
+}
+
+
+//structure
+//dictionaryEntity
+//  hws
+//    hw
+//    recordingsAndTranscriptions
+//    dictionaryEntryHeaderAdditionalInformation
+//
+//  partOfSpeechSectionHeader
+//    partOfSpeech
+//
+//  foreignToNativeMeanings
+//
 
 // exports.translate = async (text) => {
 //     return await getTranslation(text);

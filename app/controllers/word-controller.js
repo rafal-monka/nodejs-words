7e22cd09302443757be17e2756d1854c0300a4c8
Rafal Monka <monka.rafal@gmail.com>
2020-05-20 16:04:16 +0200

    html

--- app/controllers/word-controller.js
+++ app/controllers/word-controller.js
@@ -1,4 +1,6 @@
 const pagination = require("paginate-info");
+var axios = require('axios');
+require('dotenv').config();
 const db = require("../models");
 const translator = require("../translator.js");
 
@@ -95,3 +97,107 @@ exports.getAll = async (req, res) => {
     // }
   };
 
+// Find a single List with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Words.findByPk(id)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving List with id=" + id
+      });
+    });  
+};
+
+exports.htmlone = async (req, res) => {
+  const CONST_URL = process.env.API_URL;
+  try {
+      console.log("htmlone", req.params.id);
+      axios({
+          url: CONST_URL+"/find/"+req.params.id,
+          methog: 'get'
+      }).then(response => {
+        console.log("response");
+        // console.log(response.data);
+        let data = 
+          "<html><body><meta charset=\"utf-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />"
+          +"<h1>"+response.data.phrase+"</h1>"
+          +"<h2>"+response.data.sentence+"</h2>"
+          +"<pre>"
+          +JSON.stringify(response.data,null,2)
+          +"<pre></body></html>"; 
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.write(data);
+        res.end();
+      }).catch(error => {
+          console.error('#1', error);  
+          res.status(500).send({
+              message: error
+            });
+      })
+
+  } catch (error) {
+      console.error('#2', error);
+      res.status(500).send({
+        message: error
+      });
+  }
+}
+
+
+exports.htmlpage = async (req, res) => {
+  const CONST_URL = process.env.API_URL;
+  const CONST_PAGE_SIZE = 10;
+  function navStr(page) {
+    return "/api/words/htmlpage/"+page;
+  }
+  try {
+      console.log("htmlpage", req.params.currentpage);
+      axios({
+          url: CONST_URL+"/page/",
+          methog: 'get',
+          params: {
+            pageSize: CONST_PAGE_SIZE,
+            currentPage: req.params.currentpage
+          }
+      }).then(response => {
+        console.log("response");
+        // console.log(response.data);
+        let str = "";
+        response.data.rows.forEach(element => {
+            str += "<li><a href=\"/api/words/htmlone/"+element.id+"\">"+element.phrase+"</a></li>";
+        })
+
+        let data = 
+          "<html><body><meta charset=\"utf-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />"
+          +(req.params.currentpage>1 ? "<a href=\""+navStr(+req.params.currentpage-1)+"\">&lt;</a>&nbsp;" : "")
+          +"<a href=\""+navStr(+req.params.currentpage+1)+"\">&gt;</a>"
+          +"<ul>"+str+"</ul>"
+          +"<pre>"
+          +JSON.stringify(response.data,null,2)
+          +"<pre></body></html>"; 
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.write(data);
+        res.end();
+      }).catch(error => {
+          console.error('#1', error);  
+          res.status(500).send({
+              message: error
+            });
+      })
+
+  } catch (error) {
+      console.error('#2', error);
+      res.status(500).send({
+        message: error
+      });
+  }
+
+}
--- app/routes/word-routes.js
+++ app/routes/word-routes.js
@@ -7,13 +7,22 @@ module.exports = app => {
     router.get("/", words.findAll);
 
     // Get all words
-    router.get("/page/", words.getAll);
+    router.get("/find/:id", words.findOne);
 
     // Translate
     router.get("/translate/", words.translate);
 
     // Create a new word
     router.post("/create/", words.create);
+
+    // Get all words
+    router.get("/page/", words.getAll);
+
+    // One word (HTML)
+    router.get("/htmlone/:id", words.htmlone);
+
+    // Pages (HTML)
+    router.get("/htmlpage/:currentpage", words.htmlpage);
 
     app.use('/api/words', router);
   };

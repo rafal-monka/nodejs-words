38e0c458c7772ed37c855151d585cfc8969e1155
Rafal Monka <monka.rafal@gmail.com>
2020-05-23 13:20:32 +0200

    Ng index.html app (test1)

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
  */
 require('dotenv').config();
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require('path');
 const storage = require("./app/storage.js");
@@ -12,7 +13,14 @@ const db = require("./app/models");
 db.sequelize.sync(  { force: false }  ); //!!! In development, you may need to drop existing tables and re-sync database.
 storage.test();
 
-const app = express();
+const whitelist = [
+  "http://localhost:4200", 
+];
+
+const corsOptions = {
+  origin: whitelist
+}
+const app = express(); //=all; corsOptions
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -23,9 +31,16 @@ app.get("/home", (req, res) => {
     res.json({ message: "Welcome to Words application." });
 });
 
+
+//include public folder with SPA app
 app.use(
   express.static(path.join(__dirname, 'public'))
 );
+
+app.use(cors());
+//https://stackoverflow.com/questions/56181443/how-can-i-run-angular-app-on-node-server
+// app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+
 
 require("./app/routes/word-routes")(app);
 
